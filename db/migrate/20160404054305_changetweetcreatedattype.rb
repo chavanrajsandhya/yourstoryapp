2391e7a81fdd1dfa9f1c657f646d1353f1adca96
chavanrajsandhya <chavanrajsandhya@gmail.com>
2016-04-04 13:54:12 +0530

    Delete 20160404054305_changetweetcreatedattype.rb

--- db/migrate/20160404054305_changetweetcreatedattype.rb
+++
@@ -1,9 +0,0 @@
-class Changetweetcreatedattype < ActiveRecord::Migration
-  def up
-  	change_column :tweets, :tweet_created_at, :time
-  end
-
-  def down
-  	change_column :tweets, :tweet_created_at, :datetime
-  end
-end
